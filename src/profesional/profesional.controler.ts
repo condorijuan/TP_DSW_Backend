5eab9bd301d9cd3deec705c892f53ad4b767881e
SebaContreras86 <sebastiancontreras86@hotmail.com.ar>
2024-11-07 14:35:15 -0300

    CRUD Profesionales

--- src/profesional/profesional.controler.ts
+++ src/profesional/profesional.controler.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from "express";
-//import { Profesional } from "./profesional.entity.js";
+import { orm } from "../shared/db/orm.js";
+import { Profesional } from "./profesional.entity.js";
+
+const em = orm.em
 
 function sanitizeProfesional(req: Request, res: Response, next: NextFunction) {
   req.body.sanitize = {
-    id: req.body.id,
     nombre: req.body.nombre,
-    apellido: req.body.apellido
+    apellido: req.body.apellido,
+    direccion: req.body.email,
+    telefono: req.body.telefono,
+    email: req.body.email,
+    estado: req.body.estado
   };
 
   Object.keys(req.body.sanitize).forEach(key => {
@@ -17,24 +23,77 @@ function sanitizeProfesional(req: Request, res: Response, next: NextFunction) {
   next();
 }
 
-function findAll(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function findAll(req: Request, res: Response) {
+  try {
+    const profesionales = await em.find(Profesional, {})
+    res.status(200).json({ message: 'Profesionales encontrados', data: profesionales })
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-function findOne(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function findOne(req: Request, res: Response) {
+  try {
+    const id = parseInt(req.params.id);  
+    if (isNaN(id)) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
+    const profesional = await em.findOne(Profesional, id);
+    if (profesional) {
+      res.status(200).json({ message: 'Profesional found', data: profesional });
+    } else {
+      res.status(404).json({ message: 'Profesional not found' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-function add(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function add(req: Request, res: Response) {
+  try {
+    const profesional = em.create(Profesional, req.body.sanitize);
+    await em.persistAndFlush(profesional);
+    res.status(201).json({ message: 'Profesional created', data: profesional });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-function update(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function update(req: Request, res: Response) {
+  try {
+    const id = parseInt(req.params.id); 
+    if (isNaN(id)) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
+    const profesional = await em.findOne(Profesional, id);
+    if (profesional) {
+      em.assign(profesional, req.body.sanitize);
+      await em.persistAndFlush(profesional);
+      res.status(200).json({ message: 'Profesional updated', data: profesional });
+    } else {
+      res.status(404).json({ message: 'Profesional not found' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-function remove(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function remove(req: Request, res: Response) {
+  try {
+    const id = parseInt(req.params.id); 
+    if (isNaN(id)) {
+      return res.status(400).json({ message: 'Invalid ID format' });
+    }
+    const profesional = await em.findOne(Profesional, id);
+    if (profesional) {
+      await em.removeAndFlush(profesional);
+      res.status(200).json({ message: 'Profesional deleted' });
+    } else {
+      res.status(404).json({ message: 'Profesional not found' });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 export { sanitizeProfesional, findAll, findOne, add, remove, update };
