7dbce5e2ca7a4c2cde3ff809f08016b3b2beacc0
fabrikna <fabrikna@gmail.com>
2024-11-07 17:11:30 -0300

    cruds faltantes

--- src/antecedentes/antecedentes.controler.ts
+++ src/antecedentes/antecedentes.controler.ts
@@ -11,24 +11,52 @@ async function findAll(req: Request, res: Response) {
       )
       res.status(200).json({message: 'finded all antecedentes', data: antecedentes})
   } catch (error: any) {
-    res.status(500).json({message: 'Not implemented'})
+    res.status(500).json({message: error.message})
   }
 }
 
-function findOne(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function findOne(req: Request, res: Response) {
+  try {
+    const id = Number.parseInt(req.params.id)
+    const antecedente = await em.findOneOrFail(
+      Antecedente, {id},
+      { populate: ['tipoantecedente']}    
+    )
+    res.status(200).json({ message: 'found antcedente', data: antecedente})
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-function add(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function add(req: Request, res: Response) {
+  try {
+    const antecedente = em.create(Antecedente, req.body)
+    await em.flush()
+    res.status(201).json({ message: 'antecedente created', data: antecedente})
+  } catch (error: any) {
+    res.status(500).json({ message: error.message })
+  }
 }
 
-function update(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function update(req: Request, res: Response) {
+  try {
+    const id = Number.parseInt(req.params.id)
+    const antecedenteToUpdate = await em.findOneOrFail(Antecedente, { id })
+    em.assign(antecedenteToUpdate, req.body)
+    await em.flush()
+  } catch (error: any) {
+    res.status(500).json({ message: error.message})
+  }
 }
 
-function remove(req: Request, res: Response) {
-  res.status(500).json({message: 'Not implemented'});
+async function remove(req: Request, res: Response) {
+  try {
+    const id = Number.parseInt(req.params.id)
+    const antecedente = em.getReference(Antecedente, id)
+    await em.removeAndFlush(antecedente)
+  } catch (error: any) {
+    res.status(500).json({ message: error.message})
+  }
 }
 
 export { findAll, findOne, add, remove, update };
